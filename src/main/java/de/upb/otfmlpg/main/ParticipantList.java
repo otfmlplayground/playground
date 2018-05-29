f8213b3ee05798aae349fcbfeca69c8f30483866
Mirko Juergens <mirkoj@mail.upb.de>
2018-05-29 15:05:34 +0200

    Fixes a syntax error

--- src/main/java/de/upb/otfmlpg/main/ParticipantList.java
+++ src/main/java/de/upb/otfmlpg/main/ParticipantList.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class ParticipantList {
 
 	private List<Participant> allParticipants = new ArrayList<>();
-	
+
 	public ParticipantList() {
 		allParticipants.add(new Participant("Mirko Jürgens"));
 		allParticipants.add(new Participant("Felix Weiland"));
 		allParticipants.add(new Participant("Helena Graf"));
-		allParticipants.add(new Participant("Joshua Nowack"))
+		allParticipants.add(new Participant("Joshua Nowack"));
 	}
-	
+
 	@Override
 	public String toString() {
 		return allParticipants.toString();
