c1ef38d993666c1159ab32de189b91d26317305a
Felix Weiland <fweiland@mail.upb.de>
2018-05-30 12:11:40 +0200

    Adds participant to participants list
    Updates unit test

--- src/test/java/de/upb/otfmlpg/test/IntegrationTest.java
+++ src/test/java/de/upb/otfmlpg/test/IntegrationTest.java
@@ -1,4 +1,5 @@
 package de.upb.otfmlpg.test;
+
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -14,29 +15,22 @@ import de.upb.otfmlpg.main.ParticipantList;
 @RunWith(Parameterized.class)
 public class IntegrationTest {
 
-	private String toCheck;
-	
-	private ParticipantList participants;
-	
-	public IntegrationTest (String toCheck) {
+	private final String toCheck;
+
+	private final ParticipantList participants;
+
+	public IntegrationTest(final String toCheck) {
 		this.toCheck = toCheck;
 		this.participants = new ParticipantList();
 	}
-	
+
 	@Test
 	public void test() {
-		assertTrue(participants.toString().contains(toCheck));
+		assertTrue(this.participants.toString().contains(this.toCheck));
 	}
-	
-   @Parameters(name="{0}")
-   public static Collection<String[]> participants() {
-      return Arrays.asList(new String[][] {
-         { "mirkoj" },
-         {"braunm"},
-         {"noni4"}
-//         { "fweiland" },
-//			...
-      });
-   }
 
+	@Parameters(name = "{0}")
+	public static Collection<String[]> participants() {
+		return Arrays.asList(new String[][] { { "mirkoj" }, { "braunm" }, { "noni4" }, { "fweiland" }, });
+	}
 }
